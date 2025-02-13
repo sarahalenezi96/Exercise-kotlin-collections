623d7e120db2ecf66d0fe335693b168030638393
Sarah <sarahalenezi96@outlook.com>
2025-02-13 17:12:49 +0300

    Maximum and Minimum values

---
+++ task/src/max_min.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val FloatingNums = listOf(2.5, 7.1, 3.8, 9.2, 5.6)
+    println("Largest number: " + FloatingNums.maxOrNull())
+    println("Smallest number: " + FloatingNums.minOrNull())
+
+}
