be6792fcc7c6bc75bc760fb7ec31e04d225e9960
Sarah <sarahalenezi96@outlook.com>
2025-02-13 16:55:56 +0300

    car brands arrayList done

---
+++ task/src/arraylist.kt
@@ -0,0 +1,8 @@
+fun main(){
+val CarBrands = arrayListOf("BMW","Porsche","Toyota")
+    CarBrands.add("Nissan")
+    CarBrands.add("Bently")
+    CarBrands.remove("Toyota")
+    println(CarBrands)
+
+}
